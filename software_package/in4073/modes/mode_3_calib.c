78da1753a3a408b95258606c71b142035e8f7734
Unknown <jonathan.levy@ens-rennes.fr>
2018-05-20 16:51:55 +0200

    added simple calibration function that runs continuously

--- software_package/in4073/modes/mode_3_calib.c
+++ software_package/in4073/modes/mode_3_calib.c
@@ -1,33 +1,64 @@
 #include "in4073.h"
 #include "switch_mode.h"
 
-/* 
- TODO: write the constants as #define
+/*
+Calibration functions for the state machine.
+Jonathan Lévy
 */
-static uint16_t counter;
+
 
 char mode_3_calib_CANLEAVE(uint8_t target)
 {
+    char lock = 0;
+
+    // is the joystick still ?
+    lock = (axis[ROLL] != 0) || (axis[PITCH] != 0) || (axis[YAW] != 0) || ((-(axis[LIFT] - 32767) / 2) != 0);
+
+    // is there no button pressed ?
+    lock = lock || buttons;
+    
+    return (lock == 0);
 
 }
 
 char mode_3_calib_CANENTER(uint8_t source)
 {
+    char lock = 0;
 
+    // are all the motors off ?
+    lock += (motor[0] != 0);
+    lock += (motor[1] != 0);
+    lock += (motor[2] != 0);
+    lock += (motor[3] != 0);
+
+    return (lock == 0);
 }
 
 void mode_3_calib_INIT()
 {
-
+    // nothing in particular.
 }
 
 void mode_3_calib_QUIT()
 {
-
+    // nothing in particular.
 }
 
 void mode_3_calib_RUN()
 {    
+    // get values, store them as offsets. 
+    // The board might move, so we do this repetively so that only the final value is registered.
 
+    if (is_DMP_on)
+        get_dmp_data();
+    else
+        get_raw_sensor_data();
+    
+    sax_offset = sax;
+    say_offset = say;
+    saz_offset = saz;
+    sp_offset = sp;
+    sq_offset = sq;
+    sr_offset = sr;
 
 }
