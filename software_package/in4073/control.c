f03e4b30e000d1944ac054ddb078290e03bfbe05
Unknown <jonathan.levy@ens-rennes.fr>
2018-06-05 11:03:21 +0200

    changed equation to reflex decoupling of rate and angle

--- software_package/in4073/control.c
+++ software_package/in4073/control.c
@@ -48,17 +48,11 @@ void run_filters_and_control()
 	if (mode == 5)
 	{
 		js[YAW] = p_yaw * (js[YAW]/p_yaw - __SR);
-		js[ROLL] = p_p2 * (p_p1 * (js[ROLL]/(p_p1*p_p2) - __PHI) - __SP);
-		js[PITCH] = p_p2 * (p_p1 * (js[PITCH]/(p_p1*p_p2) - __THETA) - __SQ);
-		// you can add telemetry here!
+		js[ROLL] = p_p1 * (js[ROLL]/(p_p1) - __PHI) - (p_p2 * __SP);
+		js[PITCH] = p_p1 * (js[PITCH]/(p_p1) - __THETA) - (p_p2 * __SQ);
+		// you can add telemetry here! Be careful with the number of bytes though.
 		telemetry_packet[PHI] = MSBYTE(__PHI);
 		telemetry_packet[PHI+1] = LSBYTE(__PHI);
-		// WARNING WARNING WARNING
-		// 		be careful what you want to see as telemetry:
-		//		most current variables are on 4 BYTES now !!!
-		// 		Sensors and joystick raw values are still on 2 bytes though.
-		// telemetry_packet[SETPOINT_ROLL] = MSBYTE(axis[ROLL]);
-		// telemetry_packet[SETPOINT_ROLL+1] = LSBYTE(axis[ROLL]);
 	}
 
 
