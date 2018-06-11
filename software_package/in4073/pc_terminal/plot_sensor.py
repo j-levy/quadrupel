045f2d93364f283d3d233d98d2b865e1f43e395c
Tank <anhnguyentuan1610@gmail.com>
2018-06-11 17:18:07 +0200

    Plot both sr and srf

--- software_package/in4073/pc_terminal/plot_sensor.py
+++ software_package/in4073/pc_terminal/plot_sensor.py
@@ -10,13 +10,16 @@ def animate(i):
     dataArray = pullData.split('\n')
     xar = []
     yar = []
+    y2ar = []
     for eachLine in dataArray:
         if len(eachLine)>1:
-            x,y = eachLine.split(',')
-            xar.append(x)
-            yar.append(y)
+            x,y,y2 = eachLine.split(',')
+            xar.append(int(x))
+            yar.append(int(y))
+            y2ar.append(int(y2))
     ax1.clear()
-    ax1.plot(xar,yar)
+    ax1.plot(xar,yar, color = 'b')
+    ax1.plot(xar, y2ar , color = 'r')
 
 ani = animation.FuncAnimation(fig, animate, interval=20)
 plt.show()
